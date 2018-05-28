e008fa34e8bc7eff14f32b6ff2c002a74a282457
MatanShohat <matan.shohat@gmail.com>
2018-05-28 14:34:55 +0300

    CHECKPOINT 2.2.0.5

--- linearModels.py
+++ linearModels.py
@@ -10,9 +10,6 @@
     UniformQuantizerNet
         A class containing the feed forward function: forward(self, x) and
         uniform quantization
-    SOMQuantizerNet
-        A class containing the feed forward function: forward(self, x) and
-        learning SOM quantization
     AnalogProcessNet + DigitalProcessNet
         Two classes responsible for the quantization process using two distinct
         NN for the analog and the digital processes
@@ -76,41 +73,6 @@ class UniformQuantizerNet(nn.Module):
         x = self.l4(x)
         return self.l5(x)
 
-class SOMQuantizerNet(nn.Module):
-
-    def __init__(self, codebook):
-        super(SOMQuantizerNet, self).__init__()
-        self.l1 = nn.Linear(INPUT_DIMENSION, 520)
-        # See Hardware-Limited Task-Based Quantization Proposion 3. for the
-        # choice of output features
-        self.l2 = nn.Linear(520, OUTPUT_DIMENSION)
-        self.l3 = nn.Linear(OUTPUT_DIMENSION, 240)
-        self.l4 = nn.Linear(240, 120)
-        self.l5 = nn.Linear(120, OUTPUT_DIMENSION)
-        self.q1 = LearningQuantizer.QuantizationFunction.apply
-        self.codebook = codebook
-        # self.testCodebook = Variable(self.testCodebook.float())
-        # self.testCodebook = torch.from_numpy(np.asarray(codebook))
-        self.testCodebook = codebook
-
-    def forward(self, x):
-        # update the learning codebook
-        x_numpy = x.data.numpy()
-        newCodebook = list(self.testCodebook)
-        for ii in range(0, x.size(0)):
-            for jj in range(0, x.size(1)):
-                itrVal, quantized_idx = UniformQuantizer.get_optimal_word(
-                    x_numpy[ii, jj], tuple(newCodebook))
-                # update winner codeword
-                newCodebook[quantized_idx] = newCodebook[quantized_idx] + CODEBOOK_LR*(x_numpy[ii, jj] - itrVal)
-        self.testCodebook = tuple(newCodebook)
-
-        x = self.l1(x)
-        x = self.l2(x)
-        x = self.q1(x, self.codebook)
-        x = self.l3(x)
-        x = self.l4(x)
-        return self.l5(x)
 
 class AnalogProcessNet(nn.Module):
     '''A NN performind the analog pre-processing before the quantizers
